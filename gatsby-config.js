a8cad93c7095b8538391e55c98ace32b6e858a60
Hugo Fabricio <hugo@brzdigital.com.br>
2019-08-31 15:02:04 -0300

    Cleanup starter stuff

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,8 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    title: `Hugo Fabricio`,
+    description: `Um desenvolvedor Full Stack, founder da BRZ Digital, apaixonado por tecnologia e com bastante experiência no desenvolvimento de interfaces fieis ao layout e sistemas baseados em Laravel, atualmente cursando Análise e Desenvolvimento de Sistemas na FIAP e em busca de novos desafios.`,
+    author: `@hugofabriicio`,
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
